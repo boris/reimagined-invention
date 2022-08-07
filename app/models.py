6d2854d6c7fcd972b04d5922a683d220674042ff
Boris Quiroz <boris@insert-coin.org>
2022-08-07 01:13:35 -0400

    Add book table with user relation

--- app/models.py
+++ app/models.py
@@ -6,3 +6,14 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True, nullable=False)
     name = db.Column(db.String(100), nullable=False)
     password_hash = db.Column(db.String(120))
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    genre = db.Column(db.String(255))
+    year = db.Column(db.Integer)
+    pages = db.Column(db.Integer)
+    read = db.Column(db.Boolean, default=False)
+    shared = db.Column(db.Boolean, default=False)
+    rating = db.Column(db.Integer)
+    id_user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
