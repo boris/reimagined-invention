ae3c9d36579c085822e1c5e80a0dc2f60f5b854a
Boris Quiroz <boris@insert-coin.org>
2022-08-10 21:40:31 -0400

    Import LoginManager, Blueprint and some re-org

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from os import path
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user
+
 
 # Read config file
 if path.exists('/local/config.yaml'):
@@ -13,25 +15,43 @@ else:
 
 config = yaml.load(config_file, Loader=yaml.FullLoader)
 
+
+# App init
 app = Flask(__name__)
 app.config.from_mapping(
     SECRET_KEY=config['auth']['secret_key'],
     SQLALCHEMY_DATABASE_URI=config['db']['uri'],
 )
 
+
+# DB def and init
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
 db.init_app(app)
+
+
+# Import models
 from .models import User
 
-# Load the instance config
-#app.config.from_pyfile('config.py', silent=True)
 
-# Simple page to say hello
-@app.route('/hello')
-def hello():
-    return 'Hello world!'
+# Login manager
+login_manager = LoginManager()
+login_user.login_view = 'auth.login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    # since the user_id is just the primary key of our user table, use it in the query for the user
+    return User.query.get(int(id))
+
+
+# Blueprint import
+from .main import main as main_blueprint
+app.register_blueprint(main_blueprint)
+
+from .auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint)
+
 
 if __name__ == '__main__':
     app.run()
