303c1ec7126b33b1ac9d2b54fb13259f803ca072
Boris Quiroz <boris@insert-coin.org>
2022-08-11 10:12:35 -0400

    Rename to proper column name

--- app/auth.py
+++ app/auth.py
@@ -10,15 +10,15 @@ auth = Blueprint('auth', __name__)
 @auth.route('/login', methods = ['POST'])
 def login_post():
     email = request.form.get('email')
-    password = request.form.get('password')
+    password_hash = request.form.get('password')
 
     user = User.query.filter_by(email=email).first()
-    if not user or not check_password_hash(user.password, password):
+    if not user or not check_password_hash(user.password_hash, password_hash):
         flash('Usuario o contraseña inválidos')
         return redirect(url_for('auth.login'))
 
     login_user(user)
-    return redirect(url_for('main.profile'))
+    return redirect(url_for('main.index'))
 
 
 @auth.route('/login')
@@ -36,7 +36,7 @@ def logout():
 def signup_post():
     name = request.form.get('name')
     email = request.form.get('email')
-    password = request.form.get('password')
+    password_hash = request.form.get('password')
 
     user = User.query.filter_by(email=email).first()
     if user:
@@ -46,12 +46,12 @@ def signup_post():
     # create the new user with the form data
     new_user = User(name=name,
                     email=email,
-                    password=generate_password_hash(password, method='sha256'),
+                    password_hash=generate_password_hash(password, method='sha256'),
                     )
 
     # add the new user to the db
     db.session.add(new_user)
-    db.session.commt()
+    db.session.commit()
 
     return redirect(url_for('auth.login'))
 
