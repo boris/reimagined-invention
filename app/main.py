ee76e0ddf423939285b3cabe26fec435da110fcb
Boris Quiroz <boris@insert-coin.org>
2022-08-14 00:09:52 -0400

    Redirect to main.login if user is not authenticated

--- app/main.py
+++ app/main.py
@@ -13,30 +13,34 @@ def index():
 
 @main.route('/profile')
 def profile():
+    if current_user.is_authenticated:
+        books_total = db.session.query(Book.id).filter(Book.id_user == current_user.id).count()
+        books_read = db.session.query(Book.id).filter((Book.id_user == current_user.id) & (Book.read == True)).count()
+        books_unread = db.session.query(Book.id).filter((Book.id_user == current_user.id) & (Book.read == False)).count()
+        books_shared = db.session.query(Book.id).filter((Book.id_user == current_user.id) & (Book.shared == True)).count()
 
-    books_total = db.session.query(Book.id).filter(Book.id_user == current_user.id).count()
-    books_read = db.session.query(Book.id).filter((Book.id_user == current_user.id) & (Book.read == True)).count()
-    books_unread = db.session.query(Book.id).filter((Book.id_user == current_user.id) & (Book.read == False)).count()
-    books_shared = db.session.query(Book.id).filter((Book.id_user == current_user.id) & (Book.shared == True)).count()
-
-    return render_template('profile.html',
-                           name = current_user.name,
-                           greeting = current_user.email,
-                           books_total = books_total,
-                           books_read = books_read,
-                           books_unread = books_unread,
-                           books_shared = books_shared,
-                           )
+        return render_template('profile.html',
+                               name = current_user.name,
+                               greeting = current_user.email,
+                               books_total = books_total,
+                               books_read = books_read,
+                               books_unread = books_unread,
+                               books_shared = books_shared,
+                               )
+    else:
+        return redirect(url_for('auth.login'))
 
 @main.route('/books')
 def books():
+    if current_user.is_authenticated:
+        filtered_books = db.session.query(Book.title, Book.rating, Book.genre, Author.name.label('author_name'), Editorial.name.label('editorial_name'))\
+            .join(Author)\
+            .join(Editorial)\
+            .filter((Book.id_author == Author.id) & (Book.id_editorial == Editorial.id) & (Book.id_user == current_user.id))
 
-    filtered_books = db.session.query(Book.title, Book.rating, Book.genre, Author.name.label('author_name'), Editorial.name.label('editorial_name'))\
-                        .join(Author)\
-                        .join(Editorial)\
-                        .filter((Book.id_author == Author.id) & (Book.id_editorial == Editorial.id) & (Book.id_user == current_user.id))
-
-    return render_template('books.html',
+        return render_template('books.html',
                            greeting = current_user.name,
                            books = filtered_books,
                            )
+    else:
+        return redirect(url_for('main.login'))
