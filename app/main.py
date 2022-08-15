322375a20f51bf65ca88584a68ad653d7264c2f1
Boris Quiroz <boris@insert-coin.org>
2022-08-15 10:50:11 -0400

    Improve logic to check if author/editorial already exists

--- app/main.py
+++ app/main.py
@@ -58,11 +58,26 @@ def add_book():
 @main.route('/add_book', methods = ['POST'])
 def add_book_post():
     if current_user.is_authenticated:
+        # Check author details and existence
         author_name = request.form.get("author_name")
         author_country = request.form.get("author_country")
-        author = Author(country=author_country, name=author_name)
-        db.session.add(author)
-        db.session.commit()
+
+        author_exists = Author.query.filter_by(name=author_name).first()
+
+        if not author_exists:
+            author = Author(country=author_country, name=author_name)
+            db.session.add(author)
+            db.session.commit()
+
+        # Check editorial details and existence
+        editorial_name = request.form.get("editorial_name")
+        editorial_exists = Editorial.query.filter_by(name=editorial_name).first()
+
+        if not editorial_exists:
+            editorial = Editorial(name=editorial_name)
+            db.session.add(editorial)
+            db.session.commit()
+
 
         return redirect(url_for('main.add_book'))
 
