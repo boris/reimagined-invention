fc3fd61010310cff24d2d6af55543d334d7fafcb
Boris Quiroz <boris@insert-coin.org>
2023-01-19 09:12:20 -0300

    Add form to edit books

--- app/forms.py
+++ app/forms.py
@@ -21,6 +21,25 @@ class AddBookForm(FlaskForm):
     submit = SubmitField('Agregar')
 
 
+class EditBookForm(FlaskForm):
+    book_title = StringField('Título', validators=[InputRequired()])
+    author_name = StringField('Autor', validators=[InputRequired()])
+    author_country = StringField('País')
+    editorial_name = StringField('Editorial')
+    book_genre = StringField('Género')
+    book_year = IntegerField('Año')
+    book_pages = IntegerField('Número de páginas')
+    book_is_read = RadioField('¿Leído?',
+                              choices=['Si', 'No'],
+                              validators=[InputRequired()])
+    book_is_shared = RadioField('¿Prestado?',
+                              choices=['Si', 'No'],
+                              validators=[InputRequired()])
+    book_rating = IntegerField('Puntuación', validators=[NumberRange(min=0, max=5)])
+    book_tags = StringField('Tags')
+    submit = SubmitField('Agregar')
+
+
 class LoginForm(FlaskForm):
     email = StringField('Email', [DataRequired()])
     password = PasswordField('Password', [DataRequired()])
