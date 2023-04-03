eafdbe2ef28f0aca37e4c64fcf40b6c998c58dbc
Boris Quiroz <boris@insert-coin.org>
2023-04-03 06:53:38 -0400

    Fix initial DB dump

--- reimagined_invention.sql
+++ reimagined_invention.sql
@@ -24,44 +24,6 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/ `reimagined_invention` /*!40100 DEFAULT
 USE `reimagined_invention`;
 
 --
--- Table structure for table `alembic_version`
---
-
-DROP TABLE IF EXISTS `alembic_version`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `alembic_version` (
-  `version_num` varchar(32) NOT NULL,
-  PRIMARY KEY (`version_num`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `alembic_version`
---
-
-LOCK TABLES `alembic_version` WRITE;
-/*!40000 ALTER TABLE `alembic_version` DISABLE KEYS */;
-INSERT INTO `alembic_version` VALUES ('88d6278ad594');
-/*!40000 ALTER TABLE `alembic_version` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `author`
---
-
-DROP TABLE IF EXISTS `author`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `author` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `country` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Dumping data for table `author`
 --
 
@@ -70,35 +32,6 @@ LOCK TABLES `author` WRITE;
 INSERT INTO `author` VALUES (1,'Ampuero, Fernanda','Ecuador'),(2,'Aira, César','Chile'),(3,'Almada, Selva','Argentina'),(4,'Baricco, Alessandro','Italia'),(5,'Bronte, Emily','Reino Unido');
 /*!40000 ALTER TABLE `author` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `book`
---
-
-DROP TABLE IF EXISTS `book`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `book` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
-  `genre` varchar(255) DEFAULT NULL,
-  `year` int(11) DEFAULT NULL,
-  `pages` int(11) DEFAULT NULL,
-  `read` tinyint(1) DEFAULT NULL,
-  `shared` tinyint(1) DEFAULT NULL,
-  `rating` int(11) DEFAULT NULL,
-  `id_user` int(11) NOT NULL,
-  `id_author` int(11) NOT NULL,
-  `id_editorial` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_author` (`id_author`),
-  KEY `id_editorial` (`id_editorial`),
-  KEY `id_user` (`id_user`),
-  CONSTRAINT `book_ibfk_1` FOREIGN KEY (`id_author`) REFERENCES `author` (`id`),
-  CONSTRAINT `book_ibfk_2` FOREIGN KEY (`id_editorial`) REFERENCES `editorial` (`id`),
-  CONSTRAINT `book_ibfk_3` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `book`
@@ -111,23 +44,6 @@ INSERT INTO `book` VALUES (1,'Pelea de Gallos','Cuentos',2018,115,1,0,5,1,1,27),
 UNLOCK TABLES;
 
 --
--- Table structure for table `books_tags`
---
-
-DROP TABLE IF EXISTS `books_tags`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `books_tags` (
-  `id_tag` int(11) NOT NULL,
-  `id_book` int(11) NOT NULL,
-  PRIMARY KEY (`id_tag`,`id_book`),
-  KEY `id_book` (`id_book`),
-  CONSTRAINT `books_tags_ibfk_1` FOREIGN KEY (`id_book`) REFERENCES `book` (`id`),
-  CONSTRAINT `books_tags_ibfk_2` FOREIGN KEY (`id_tag`) REFERENCES `tag` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Dumping data for table `books_tags`
 --
 
@@ -135,20 +51,6 @@ LOCK TABLES `books_tags` WRITE;
 /*!40000 ALTER TABLE `books_tags` DISABLE KEYS */;
 /*!40000 ALTER TABLE `books_tags` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `editorial`
---
-
-DROP TABLE IF EXISTS `editorial`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `editorial` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `editorial`
@@ -161,20 +63,6 @@ INSERT INTO `editorial` VALUES (1,'ááéé'),(2,'asdf'),(3,'asdfááééhola'),
 UNLOCK TABLES;
 
 --
--- Table structure for table `tag`
---
-
-DROP TABLE IF EXISTS `tag`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tag` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`,`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Dumping data for table `tag`
 --
 
@@ -182,23 +70,6 @@ LOCK TABLES `tag` WRITE;
 /*!40000 ALTER TABLE `tag` DISABLE KEYS */;
 /*!40000 ALTER TABLE `tag` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(100) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `password_hash` varchar(120) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `user`
